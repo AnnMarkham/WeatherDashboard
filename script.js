aa8e4b0e06ddc661404c36aef2f134bd34ca9a12
Ann Markham <ann.markham@outlook.com>
2020-10-26 14:34:07 -0700

    row1 search bos and button updated

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+schedule = JSON.parse(localStorage.getItem("schedule"));
+if (!schedule) {
+  schedule = [];
+};
+
+
+
+
+
+
+//save button function
+$(".saveBtn").on("click", function () {
+  schedule = []
+  for (i = 0; i < 9; i++) {
+    schedule.push($("#text" + i).val());
+  }
+
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+});
+
+// save function
+var saveAppts = function () {
+  schedule = []
+  for (i = 0; i < 9; i++) {
+    schedule.push($("#text" + i).val());
+  }
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+}
