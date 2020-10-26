5b0bd15359e2b4787886737c7128dc5973ddc90a
Ann Markham <ann.markham@outlook.com>
2020-10-26 16:41:01 -0700

    event handler for submit button created and recognized by browser

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 
 
+var searchFormEl = document.querySelector("#searchForm");
+var searchInputEl = document.querySelector("#search");
 
-
-
-
-
-schedule = JSON.parse(localStorage.getItem("schedule"));
-if (!schedule) {
-  schedule = [];
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+  //add details about what this function will do in addition to preventing default
+  //ie add cityname to history
+  //search for cityname in weather api
 };
 
+searchFormEl.addEventListener("submit", formSubmitHandler);
 
 
 
 
 
-//save button function
-$(".saveBtn").on("click", function () {
-  schedule = []
-  for (i = 0; i < 9; i++) {
-    schedule.push($("#text" + i).val());
-  }
+// schedule = JSON.parse(localStorage.getItem("schedule"));
+// if (!schedule) {
+//   schedule = [];
+// };
 
-  localStorage.setItem("schedule", JSON.stringify(schedule));
-});
 
-// save function
-var saveAppts = function () {
-  schedule = []
-  for (i = 0; i < 9; i++) {
-    schedule.push($("#text" + i).val());
-  }
-  localStorage.setItem("schedule", JSON.stringify(schedule));
-}
+
+
+
+
+// //save button function
+// $(".saveBtn").on("click", function () {
+//   schedule = []
+//   for (i = 0; i < 9; i++) {
+//     schedule.push($("#text" + i).val());
+//   }
+
+//   localStorage.setItem("schedule", JSON.stringify(schedule));
+// });
+
+// // save function
+// var saveAppts = function () {
+//   schedule = []
+//   for (i = 0; i < 9; i++) {
+//     schedule.push($("#text" + i).val());
+//   }
+//   localStorage.setItem("schedule", JSON.stringify(schedule));
+// }
