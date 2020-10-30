b493b2996883658be61ac0b9770ae82d0683b1b1
Ann Markham <ann.markham@outlook.com>
2020-10-30 12:18:12 -0700

    reset to last functioning JSON fetch

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-$("#searchBtn").on("click", function () {
-  var cityValue = $(".form-input").val();
-  console.log(cityValue);
-})
+var getCityWeather = function (city) {
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=c5f163a6903bc47e2936fd40702fea5f"
+  //make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+
+    });
+  });
+};
+var searchFormEl = document.querySelector("#searchForm");
+var searchInputEl = document.querySelector("#city");
+var formSubmitHandler = function (event) {
+  event.preventDefault();//prevents browser from sending info to url.  need to create a way to do this separately, or figure out how to connect this to submit request for weather Api
+  var city = searchInputEl.value.trim();
+  if (city) {
+    getCityWeather(city);
+    searchInputEl.value = "";
+  }
+  else
+    alert("Please enter a valid US city name")
+};
+searchFormEl.addEventListener("submit", formSubmitHandler);
