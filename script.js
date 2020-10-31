15d186bbe20dde233740a8a992bc0ce05fa5ab39
Ann Markham <ann.markham@outlook.com>
2020-10-30 17:25:09 -0700

    page is ugly but today weather card populates and search button works

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var getCityWeather = function (city) {
-  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=c5f163a6903bc47e2936fd40702fea5f"
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=c5f163a6903bc47e2936fd40702fea5f"
   //make a request to the url
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
 
-      console.log("Name:", data.name)
+      console.log(data)
+
       $(".card-header").text(data.name)
+      var tbody = $(".todayCardBody tbody").empty();
+      tbody.append("<tr><th>Temperature:</th><td>" + data.main.temp + " Degrees F" + "</td></tr>");
+      tbody.append("<tr><th>Humidity:</th><td>" + data.main.humidity + "%" + "</td></tr>");
+      tbody.append("<tr><th>Wind Speed:</th><td>" + data.wind.speed + "MPH" + "</td></tr>");
+      tbody.append("<tr><th>UV Index: </th><td>" + "PlaceHolder" + "</td></tr>");
     })
   })
 };
